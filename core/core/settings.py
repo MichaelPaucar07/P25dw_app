f91215831e43e3e05ae48712109d75c964236884
MichaelPaucar07 <117292792+MichaelPaucar07@users.noreply.github.com>
2025-11-13 18:53:12 -0500

    Implement connection pooling and SQL composition utilities in psycopg2
    - Added connection pooling functionality with AbstractConnectionPool, SimpleConnectionPool, and ThreadedConnectionPool classes.
    - Introduced Composable, Composed, SQL, Identifier, Literal, and Placeholder classes for SQL string composition.
    - Implemented FixedOffsetTimezone and LocalTimezone classes for timezone handling.
    - Enhanced error handling and connection management in the pooling classes.
    - Improved SQL composition capabilities with support for placeholders and formatting.

--- core/core/settings.py
+++ core/core/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'practice25dw',
+        'USER': 'practiusr',
+        'PASSWORD': 'pract2025pwd',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
