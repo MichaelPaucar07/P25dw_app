a16b3c5c5041120da7fd052f6e299d18edfe16cb
MichaelPaucar07 <117292792+MichaelPaucar07@users.noreply.github.com>
2025-11-13 19:19:34 -0500

    Add Docente model with fields for name, email, ID, blood type, address, and image

--- core/apps/docentes/models.py
+++ core/apps/docentes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Docente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correo = models.EmailField(unique=True)
+    cedula = models.CharField(max_length=10, unique=True)
+    tipo_sangre = models.CharField(max_length=5)
+    direccion = models.TextField()
+    imagen = models.ImageField(upload_to='docentes/')
+    
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
