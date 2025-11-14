4853d51632830995914a8b95c7a38c9ccfd856c7
MichaelPaucar07 <117292792+MichaelPaucar07@users.noreply.github.com>
2025-11-13 19:22:40 -0500

    Add Curso model with fields for name, credits, level, weekly hours, shift, image, and related docentes

--- core/apps/materias/models.py
+++ core/apps/materias/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from core.apps.docentes.models import Docente
 
-# Create your models here.
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    numero_creditos = models.IntegerField()
+    nivel = models.CharField(max_length=20)
+    horas_semana = models.IntegerField()
+    jornada = models.CharField(max_length=20)
+    imagen = models.ImageField(upload_to='cursos/')
+    docentes = models.ManyToManyField(Docente, related_name='cursos')
+    
+    def __str__(self):
+        return self.nombre
